70283ae7be0b1323a9d96ef86e5dd0f980baba21
Aba Yusupov <rivojuy@gmail.com>
2024-10-27 14:45:09 +0500

    Add Express server for deployment

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Serve static files from the React app
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", (req, res) => {
